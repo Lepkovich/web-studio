0d385bc40558373c17888053c0fc6013d4e766a6
Lepkovich <6332133@gmail.com>
2024-01-28 19:05:31 +0300

    login and signup works

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../../core/auth/auth.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {HttpErrorResponse} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  isLogged: boolean = false;
+
+  constructor(private authService: AuthService,
+              private _snackBar: MatSnackBar,
+              private router: Router,
+  ) {
+    this.isLogged = authService.isLoggedIn();
+    console.log(this.isLogged)
+  }
+
+  ngOnInit() {
+    this.authService.isLogged$
+      .subscribe((isLoggedIn: boolean) => {
+        this.isLogged = isLoggedIn;
+      });
+  }
+
+  logout(): void {
+    this.authService.logout()
+      .subscribe({
+        next: () => {
+          this.doLogout();
+        },
+        error: (errorResponse: HttpErrorResponse) => {
+          this.doLogout();
+        }
+      })
+  }
+
+  doLogout(): void {
+    this.authService.removeTokens();
+    this.authService.userId = null;
+    this._snackBar.open('Вы вышли из системы')
+    this.router.navigate(['/']);
+  }
 }
