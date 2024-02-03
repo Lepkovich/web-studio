5f258504eb53008e881164552cca5142f49730f7
Lepkovich <6332133@gmail.com>
2024-02-03 15:44:03 +0300

    reviews carousel done

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ArticleService} from "../../shared/services/article.service";
 import {ArticlesCardType} from "../../../types/articles-card.type";
+import {OwlOptions} from "ngx-owl-carousel-o";
 
 @Component({
   selector: 'app-main',
@@ -10,6 +11,80 @@ import {ArticlesCardType} from "../../../types/articles-card.type";
 export class MainComponent implements OnInit{
 
   articles: ArticlesCardType[] = [];
+
+  reviews = [
+    {
+      name: 'Станислав',
+      image: 'review-1.png',
+      text: 'Спасибо огромное АйтиШторму за прекрасный блог с полезными статьями! Именно они и побудили меня углубиться в тему SMM и начать свою карьеру.'
+    },
+    {
+      name: 'Алёна',
+      image: 'review-2.png',
+      text: 'Обратилась в АйтиШторм за помощью копирайтера. Ни разу ещё не пожалела! Ребята действительно вкладывают душу в то, что делают, и каждый текст, который я получаю, с нетерпением хочется выложить в сеть.'
+    },
+    {
+      name: 'Мария',
+      image: 'review-3.png',
+      text: 'Команда АйтиШторма за такой короткий промежуток времени сделала невозможное: от простой фирмы по услуге продвижения выросла в мощный блог о важности личного бренда. Класс!'
+    },
+    {
+      name: 'Станислав',
+      image: 'review-1.png',
+      text: 'Спасибо огромное АйтиШторму за прекрасный блог с полезными статьями! Именно они и побудили меня углубиться в тему SMM и начать свою карьеру.'
+    },
+    {
+      name: 'Алёна',
+      image: 'review-2.png',
+      text: 'Обратилась в АйтиШторм за помощью копирайтера. Ни разу ещё не пожалела! Ребята действительно вкладывают душу в то, что делают, и каждый текст, который я получаю, с нетерпением хочется выложить в сеть.'
+    },
+    {
+      name: 'Мария',
+      image: 'review-3.png',
+      text: 'Команда АйтиШторма за такой короткий промежуток времени сделала невозможное: от простой фирмы по услуге продвижения выросла в мощный блог о важности личного бренда. Класс!'
+    },
+  ]
+
+  customOptionsSlider: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    margin: 0,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+    },
+    nav: true
+  }
+
+
+  customOptionsReviews: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    margin: 25,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      }
+    },
+    nav: false
+  }
 
   constructor(private articleService: ArticleService) {
   }
