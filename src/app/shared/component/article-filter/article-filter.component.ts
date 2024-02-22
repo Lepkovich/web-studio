3b744fdc465f36096cbfcdda55fce2410d7e3c80
Lepkovich <6332133@gmail.com>
2024-02-22 18:52:21 +0300

    fix bug with pushing queryParams. removeAppliedFilter() works

--- src/app/shared/component/article-filter/article-filter.component.ts
+++ src/app/shared/component/article-filter/article-filter.component.ts
@@ -2,42 +2,48 @@ import {Component, Input, OnInit} from '@angular/core';
 import {CategoriesType} from "../../../../types/categories.type";
 import {ActiveParamsType} from "../../../../types/active-params.type";
 import {ActivatedRoute, Router} from "@angular/router";
-import {FilteredCategoriesType} from "../../../../types/filtered-categories.type";
 
 @Component({
   selector: 'article-filter',
   templateUrl: './article-filter.component.html',
   styleUrls: ['./article-filter.component.scss']
 })
-export class ArticleFilterComponent implements OnInit{
+
+export class ArticleFilterComponent implements OnInit {
 
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute) {
   }
 
 
-
-  @Input() categories:CategoriesType[] = [];
+  @Input() categories: CategoriesType[] = [];
   open = false;
-  activeParams: ActiveParamsType = {pages: 1, categories: []};
-
+  activeParams: ActiveParamsType = {categories: []};
 
 
   ngOnInit() {
+
     this.activatedRoute.queryParams.subscribe(params => {
-      // const categoriesFromQuery = params['categories']; //взяли из queryParams только categories
-      this.activeParams.categories = params['categories'];
 
-      // this.filteredCategories = this.categories.filter(category => categoriesFromQuery.includes(category.url)); //отфильтровали categories
-      //добавили active=true выбранным категориям
+        this.activeParams.categories = params['categories'] || []; // Установка значения по умолчанию
 
-      // console.log('categoriesFromQuery', categoriesFromQuery);
-      // console.log('filteredCategories', this.filteredCategories);
+      if (this.categories && this.categories.length > 0) {
+        this.categories.forEach(category => {
+          category.active = this.activeParams.categories.includes(category.url);
+        });
+      }
 
-      // this.updateFilter(categoriesFromQuery);
-
+      // возможно понадобится такой подход для подготовки запроса на бэкенд
+      // const activeParams : ActiveParamsType = {categories: []};
+      // if (params.hasOwnProperty('categories')) {
+      //   this.activeParams.categories = Array.isArray(params['categories']) ? params['categories'] : [params['categories']];
+      // }
+      // if (params.hasOwnProperty('pages')) {
+      //   this.activeParams.pages = +params['pages']; //+ чтобы сразу конвертировать в цифру
+      // }
+      //
+      // this.activeParams = activeParams;
     })
-    console.log('ngOnInit');
   }
 
   toggle() {
@@ -53,15 +59,13 @@ export class ArticleFilterComponent implements OnInit{
 
   updateFilter(category: string) {
 
-    console.log('category', category);
-    console.log('this.activeParams.categories', this.activeParams.categories);
-
-    if(this.activeParams.categories && this.activeParams.categories.length > 0) {
+    if (this.activeParams.categories && this.activeParams.categories.length > 0) {
       const existingCategoryParams = this.activeParams.categories.find(item => item === category)
       if (existingCategoryParams) {
         this.activeParams.categories = this.activeParams.categories.filter(item => item !== category)
       } else if (!existingCategoryParams) {
-        this.activeParams.categories.push(category);
+        // this.activeParams.categories.push(category); //так не работало из-за бага Angular
+        this.activeParams.categories = [...this.activeParams.categories, category]; //решается через создание копии и добавления нового значения
       }
     } else {
       this.activeParams.categories = [category]
@@ -77,12 +81,13 @@ export class ArticleFilterComponent implements OnInit{
       });
     }
 
-    console.log('this.activeParams.categories after if', this.activeParams.categories);
-    console.log('this.activeParams after if', this.activeParams);
+    const queryParams = {
+      pages: this.activeParams.pages,
+      categories: this.activeParams.categories
+    };
 
-    this.router.navigate(['/catalog'], {
-      queryParams: this.activeParams
-    })
+    this.router.navigate(['/catalog'], {queryParams: queryParams});
+
   }
 
 }
--- src/app/views/blog/catalog/catalog.component.ts
+++ src/app/views/blog/catalog/catalog.component.ts
@@ -4,6 +4,7 @@ import {ArticlesCardType} from "../../../../types/articles-card.type";
 import {CategoriesType} from "../../../../types/categories.type";
 import {ActivatedRoute, Router} from "@angular/router";
 import {FilteredCategoriesType} from "../../../../types/filtered-categories.type";
+import {ActiveParamsType} from "../../../../types/active-params.type";
 
 @Component({
   selector: 'app-catalog',
@@ -17,6 +18,8 @@ export class CatalogComponent implements OnInit{
   categories: CategoriesType[] = [];
   pages: number = 1;
   filteredCategories: FilteredCategoriesType[] = [];
+  activeParams: ActiveParamsType = {categories: []};
+
 
 
   constructor(private articleService: ArticleService,
@@ -35,10 +38,10 @@ export class CatalogComponent implements OnInit{
         this.categories = data;
         if (this.categories && this.categories.length > 0) {
           this.activatedRoute.queryParams.subscribe(params => {
-            const categoriesFromQuery = params['categories']; //взяли из queryParams только categories
-            if (categoriesFromQuery && categoriesFromQuery.length > 0) {
-              this.filteredCategories = this.categories.filter(category => categoriesFromQuery.includes(category.url)); //отфильтровали categories
-              console.log('filteredCategories', this.filteredCategories)
+            this.activeParams.categories = params['categories']; //взяли из queryParams только categories
+            console.log('this.activeParams', this.activeParams)
+            if (this.activeParams.categories && this.activeParams.categories.length > 0) {
+              this.filteredCategories = this.categories.filter(category => this.activeParams.categories.includes(category.url)); //отфильтровали categories
             } else {
               this.filteredCategories = [];
             }
@@ -50,7 +53,17 @@ export class CatalogComponent implements OnInit{
   }
 
   removeAppliedFilter(appliedFilter: FilteredCategoriesType) {
-    console.log('appliedFilter', appliedFilter)
+    this.activeParams.categories = this.activeParams.categories.filter(item => item !== appliedFilter.url);
+
+    const queryParams = {
+      pages: this.activeParams.pages,
+      categories: this.activeParams.categories
+    };
+
+    this.router.navigate(['/catalog'], {queryParams: queryParams});
+
+    console.log('appliedFilter', appliedFilter);
+    console.log('this.activeParams.categories', this.activeParams.categories)
   }
 
 
